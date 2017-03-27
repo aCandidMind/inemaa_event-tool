15a38607e5f371a01c403a3a283b2cfdb77a7dd6
aCandidMind <opensource@candidminds.com>
2017-03-27 09:21:55 +0200

    Added better splash styling

--- src/splash.js
+++ src/splash.js
@@ -4,17 +4,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Styling
+import { FlexParent, FlexChild } from 'react-foundation-components/lib/flex';
 import './splash.css';
+import './logo.png';
+import './splashFilters.jpg';
 
 import 'react-foundation-components/lib/_typography.scss';
 
 const App = () => {
   return (
-    <div id="splashContainer">
-      <a href="http://localhost:3000/#mapSummer">
-        <img id="map" role="presentation" src="http://localhost:3000/map_summer.jpg" />
-      </a>
-    </div>
+    <FlexParent id="splashContainer" horizontalAlignment="middle" verticalAlignment="middle">
+      <FlexChild>
+        <a href="http://localhost:3000/#mapSummer">
+          <img id="map" role="presentation" src="http://localhost:3000/splashFilters.jpg" />
+        </a>
+      </FlexChild>
+    </FlexParent>
   );
 };
 
