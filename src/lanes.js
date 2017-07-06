9e07f7a63e941c1fb673e546a594723e6afc438c
aCandidMind <opensource@candidminds.com>
2017-07-06 19:42:19 +0200

    Add id to all data objects and associations to locations, render only associated caterings on each location click ...
    This doesn't work yet, it basically should and the logged objects look decent, but the re-rendering of the
    Trello Board doesn't seem to happen.

--- src/lanes.js
+++ src/lanes.js
@@ -9,32 +9,37 @@ import './lanes.css';
 import { Board } from 'react-trello';
 
 const data = require("json!./data.json");
+const firstLocation = data.location[0];
 import CardHeader from './cardHeader';
 import CardContent from './cardContent';
 
 class Lanes extends Component {
 
+  state = {
+    cardId: firstLocation.id,
+    cardMetadata: {associations: firstLocation.associations}
+  };
+
   render() {
+    console.log("render", this.state.cardId, this.state.cardMetadata.associations.catering.map((c) => c.id));
     return (
       <ClearFix id="lanes">
         <Float position="center" noWrap>
-          <Board data={this.enrichData(data)} draggable={false} />
+          <Board
+            data={this.enrichData(data)}
+            onCardClick={(cardId, cardMetadata) => this.handleCardClick(cardId, cardMetadata)}
+            draggable={false} />
         </Float>
       </ClearFix>
     );
   }
 
-  getCardTitleMarkup(id, title) {
-    return <CardHeader id={id} title={title} />;
-  }
-
-  getCardLabelMarkup(id, card) {
-    const label = [<strong key={id + '_fp'}>{card.footprint}</strong>, ' kg CO2/Gast'];
-    return <CardHeader id={id} className="cardLabel" label={label} />;
-  }
-
-  getContentMarkup(kind, card) {
-    return <CardContent kind={kind} card={card} />;
+  handleCardClick(cardId, cardMetadata) {
+    console.log("handleCardClick", cardId);
+    if (cardMetadata.type === 'location') {
+      console.log(cardMetadata.associations.catering.map((c) => c.id));
+      this.setState({cardId, cardMetadata});
+    }
   }
 
   enrichData(data) {
@@ -42,23 +47,54 @@ class Lanes extends Component {
       lanes: [
       ]
     };
+    const associations = this.state.cardMetadata && this.state.cardMetadata.associations || [];
     Object.keys(data).forEach((key) => {
       const lane = {id: key, title: key.toUpperCase(), cards: []};
-      data[key].forEach((item, i) => {
-        const id = `${key}_${i}`;
+      data[key].forEach((item) => {
+        if (key != "location" && !findAssociationById(associations, key, item.id)) {
+          return;
+        }
+
+        const id = `${key}_${item.id}`;
         const card = {
           id: id,
-          title: this.getCardTitleMarkup(id, item.title),
-          description: this.getContentMarkup(key, item),
-          label: this.getCardLabelMarkup(id, item),
-          metadata: {cssClassname: `searchresult ${item.color}`},
+          title: getCardTitleMarkup(id, item.title),
+          description: getContentMarkup(key, item),
+          label: getCardLabelMarkup(id, item),
+          metadata: {
+            associations: item.associations,
+            type: key,
+            cssClassname: `searchresult ${item.color}`
+          },
         };
         lane.cards.push(card);
       });
       enrichedData.lanes.push(lane);
     });
+    console.log("enrichedData", enrichedData);
     return enrichedData;
   }
 }
 
+function getCardTitleMarkup(id, title) {
+  return <CardHeader id={id} title={title} />;
+}
+
+function getCardLabelMarkup(id, card) {
+  const label = [<strong key={id + '_fp'}>{card.footprint}</strong>, ' kg CO2/Gast'];
+  return <CardHeader id={id} className="cardLabel" label={label} />;
+}
+
+function getContentMarkup(kind, card) {
+  return <CardContent kind={kind} card={card} />;
+}
+
+function findAssociationById(associations, key, id) {
+  if (!associations[key] || !associations[key].length) {
+    return null;
+  }
+  return associations[key].find(function (association) {
+    return association.id === id;
+  })
+}
 export default Lanes;
