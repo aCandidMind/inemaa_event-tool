0a215db7a2f2743f6744839ad0c8b1ae691c5c51
aCandidMind <opensource@candidminds.com>
2017-08-21 07:29:15 +0200

    Fix search result styling for full width by using relative+absolute positioning
    Also got rid of two extra levels of tag nesting in card content markup

--- src/cardContent.js
+++ src/cardContent.js
@@ -5,8 +5,7 @@ import { Label } from 'react-foundation-components/lib/label';
 import { Button } from 'react-foundation-components/lib/button';
 import { Reveal } from 'react-foundation-components/lib/reveal';
 import { CloseButton } from 'react-foundation-components/lib/close-button';
-import { Float, ClearFix } from 'react-foundation-components/lib/float';
-import { FlexParent, FlexChild } from 'react-foundation-components/lib/flex';
+import { ClearFix } from 'react-foundation-components/lib/float';
 import 'font-awesome/scss/font-awesome.scss';
 
 import * as detailPages from './detailPages';
@@ -27,7 +26,7 @@ class CardContent extends Component {
       distanceStation,
     } = this.props.card.metadata;
     return (
-      <Float position="left" className="searchMetadata">
+      <div className="searchMetadata">
         {this.props.kind === 'location' &&
           <div>
             <div>Kapazität: {this.props.card.metadata.capacity}</div>
@@ -38,7 +37,7 @@ class CardContent extends Component {
           <li><span className="fa fa-dot-circle-o" /> {distanceCenter}</li>
           <li><span className="fa fa-train" /> {distanceStation}</li>
         </ul>
-      </Float>
+      </div>
     );
   }
 
@@ -52,42 +51,41 @@ class CardContent extends Component {
       showDetail,
     } = this.state;
 
+    const dataAndButtonsExtraClass = kind === 'location' ? ' extraMetadata' : '';
+
     return (
-      <div>
+      <ClearFix>
         <div className="resultTags">
           {card.tags.map(tag => <Label key={tag} color="primary">{tag}</Label>)}
         </div>
-        <ClearFix>
-          <FlexParent verticalAlignment="bottom">
-            {this.getListFormatMetaData()}
-            <Float className="resultButtons" position="right">
-              <FlexChild>
-                <Button
-                  className="moreButton"
-                  color="secondary"
-                  size="small"
-                  onClick={() => {}}>
-                  <i className="fa fa-save"></i>
-                  merken
-                </Button>
-                <Button
-                  className="moreButton"
-                  color="secondary"
-                  size="small"
-                  onClick={this.handleShowDetail}>
-                  <span className="fa fa-info"></span>
-                  mehr ...
-                </Button>
-                <Reveal onHide={this.handleHideDetail} show={showDetail}>
-                  <CloseButton onClick={this.handleHideDetail} />
-                  <h1>{card.title} <small>{kind}</small></h1>
-                  <img role="presentation" src={detailPages[kind]} />
-                </Reveal>
-              </FlexChild>
-            </Float>
-          </FlexParent>
-        </ClearFix>
-      </div>
+        <div className={`dataAndButtons${dataAndButtonsExtraClass}`}>
+          {this.getListFormatMetaData()}
+          <div className="resultButtons">
+            <Button
+              className="moreButton"
+              color="secondary"
+              size="small"
+              onClick={() => {}}>
+              <i className="fa fa-save"></i>
+              merken
+            </Button>
+            <Button
+              className="moreButton"
+              color="secondary"
+              size="small"
+              onClick={this.handleShowDetail}>
+              <span className="fa fa-info"></span>
+              mehr ...
+            </Button>
+            <Reveal onHide={this.handleHideDetail} show={showDetail}>
+              <CloseButton onClick={this.handleHideDetail} />
+              <h1>{card.title} <small>{kind}</small></h1>
+              <img role="presentation" src={detailPages[kind]} />
+            </Reveal>
+          </div>
+          <ClearFix/>
+        </div>
+      </ClearFix>
     );
   }
 }
