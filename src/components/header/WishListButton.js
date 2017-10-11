fa1bbe3be783fa857a699749748100b3ab4c5bdb
aCandidMind <opensource@candidminds.com>
2017-10-11 14:59:52 +0200

    green is success

--- src/components/header/WishListButton.js
+++ src/components/header/WishListButton.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class WishListButton extends Component {
   render() {
     return (
-      <button className="wish-list-button button success" type="button">
+      <button className="wish-list-button button secondary" type="button">
         <i className="fa fa-2x fa-paperclip"></i>
         Merkzettel
         <span className="right">0</span>
