50cd2cd7a7903244765e55f89740d9ab34c28855
aCandidMind <opensource@candidminds.com>
2017-10-19 05:05:59 +0200

    Fix carousel button function and styling (inactive when at the end)

--- src/components/records/RecordCarousel.js
+++ src/components/records/RecordCarousel.js
@@ -1,36 +1,47 @@
 import React, {Component} from 'react';
 import Carousel from 'nuka-carousel';
 
-const decorators = [
-  {
-    component: function() {
-      if (!this._carousel) { return null; }
-      return (
-        <button className="slide-button slide-prev" onClick={this._carousel.previousSlide}>
-          <span className="fa fa-arrow-left" />
-        </button>
-      )
+function getDecorators() {
+  return [
+    {
+      component: function(props) {
+        if (!this._carousel) { return null; }
+        let classNames = "slide-button slide-prev";
+        if (props.currentSlide === 0) {
+          classNames += ' inactive';
+        }
+        return (
+          <button className={classNames} onClick={this._carousel.previousSlide}>
+            <span className="fa fa-arrow-left" />
+          </button>
+        )
+      },
+      position: 'CenterLeft',
     },
-    position: 'CenterLeft',
-  },
-  {
-    component: function() {
-      if (!this._carousel) { return null; }
-      return (
-        <button className="slide-button slide-next" onClick={this._carousel.nextSlide}>
-          <span className="fa fa-arrow-right" />
-        </button>
-      )
+    {
+      component: function(props) {
+        if (!this._carousel) { return null; }
+        let classNames = "slide-button slide-next";
+        if (props.currentSlide + props.slidesToScroll >= props.slideCount - props.slidesToShow + 1) {
+          classNames += ' inactive';
+        }
+        return (
+          <button className={classNames} onClick={this._carousel.nextSlide}>
+            <span className="fa fa-arrow-right" />
+          </button>
+        )
+      },
+      position: 'CenterRight',
     },
-    position: 'CenterRight',
-  },
-];
+  ];
+};
 
 class RecordCarousel extends Component {
 
   constructor(props) {
     super(props);
-    decorators.forEach(decorator => {
+    this.decorators = getDecorators();
+    this.decorators.forEach(decorator => {
        decorator.component = decorator.component.bind(this);
     });
   }
@@ -42,7 +53,7 @@ class RecordCarousel extends Component {
         slidesToScroll={1}
         initialSlideHeight={288}
         cellSpacing={20}
-        decorators={decorators}
+        decorators={this.decorators}
         ref={(c) => this._carousel = c}
       >
         {this.props.records}
