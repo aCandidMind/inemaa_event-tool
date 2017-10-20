5cec714ba745a144e4859e760a710544b937bc85
aCandidMind <opensource@candidminds.com>
2017-10-20 09:32:06 +0200

    Add header to homepage (made header more flexible for this)

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Header from './components/header/Header';
+
 
 class Home extends Component {
   render() {
     return (
       <div id="home">
+        <Header noLogo={true} />
         <a href="/app">App</a>
       </div>
     );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -16,9 +16,12 @@ class Header extends Component {
         <div className="cell" >
           {/* mobile nav bar */}
           <div className="title-bar topbar-center-logo-mobile hide-for-medium" data-responsive-toggle="topbar-center-logo">
-            <div className="title-bar-left">
-              <div className="title-bar-title"><Logo /></div>
-            </div>
+            {
+              this.props.noLogo !== true &&
+                <div className="title-bar-left">
+                  <div className="title-bar-title"><Logo /></div>
+                </div>
+            }
             <div className="title-bar-center">
               <WishListButton />
             </div>
@@ -30,9 +33,12 @@ class Header extends Component {
 
           {/* medium and larger nav bar */}
           <div className="top-bar topbar-center-logo hide-for-small" id="topbar-center-logo">
-            <div className="top-bar-left">
-              <Logo />
-            </div>
+            {
+              this.props.noLogo !== true &&
+                <div className="top-bar-left">
+                  <Logo />
+                </div>
+            }
             <div className="top-bar-center">
               <ul className="menu vertical medium-horizontal hide-for-small">
                 <li><Link to="#">Login</Link></li>
@@ -41,10 +47,13 @@ class Header extends Component {
                 <li><Link to="#">Hilfe</Link></li>
               </ul>
             </div>
-            <div className="top-bar-right">
-              <WishListButton handleWishListClick={this.props.handleWishListClick}
-                              count={this.props.wishlistCount} />
-            </div>
+            {
+              this.props.handleWishListClick &&
+                <div className="top-bar-right">
+                  <WishListButton handleWishListClick={this.props.handleWishListClick}
+                                count={this.props.wishlistCount} />
+                </div>
+            }
           </div>
           {/* /medium and larger nav bar */}
         </div>
